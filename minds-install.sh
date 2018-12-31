9c7e11756d534b7f2487df2555696aa1424c54be
Serkan-devel <serkan.devel@gmail.com>
2018-12-31 17:10:25 +0000

    proper wording on minds-install.sh aswell

--- minds-install.sh
+++ minds-install.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
-echo "YOU MUST BE ROOT USER, ARE YOU ROOT!??" 
+echo "YOU MUST BE ROOT USER, ARE YOU ROOT!?? This script will fail otherwise ^" 
 apt-get install docker-compose -y
 apt-get install docker-containerd -y
 apt-get install docker-registry -y
 apt-get install docker.io -y
 apt-get install git
-echo "git Minds Source"
+echo "Downloading minds/upstream source code"
 git clone https://github.com/Minds/minds.git
 cd minds
 sh init.sh
 groupadd docker
 gpasswd -a $USER docker
-echo "Installing Minds, This could take a while..."
+echo "Deploying Minds, This could take a while..."
 docker-compose up -d nginx
 docker-compose up installer
 docker-compose up front-build
