9052d79a5e4a27d30e90921037ac28308beb35c9
Serkan-devel <serkan.devel@gmail.com>
2018-12-31 17:04:06 +0000

    proper wording on script files

--- minds-install-user.sh
+++ minds-install-user.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "Installing Minds, This could take a while..."
+echo "Deploying Minds, This could take a while..."
 docker-compose up -d nginx
 docker-compose up installer
 docker-compose up front-build
@@ -9,7 +9,7 @@ sudo apt-get upgrade -y
 sudo apt-get clean
 sudo apt-get autoclean
 sudo apt-get autoremove -y
-echo "Would you like to upgrade your distro?"
+echo "Would you like to upgrade your distro? Too bad, we'll do it anyway"
 sudo apt-get dist-upgrade
 
  
